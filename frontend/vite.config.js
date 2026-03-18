d2954add2f6f009d76913ac6b0c36ffc8c05508a
Simone Pagliuca <pagliuca.simone01@gmail.com>
2026-03-18 14:50:02 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    allowedHosts: true
   },
   test: {
     environment: 'jsdom',
